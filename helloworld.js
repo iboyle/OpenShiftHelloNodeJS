1a6f601f105b830eb53f7bc810596c9617edc973
Iain Boyle <iboyle@users.noreply.github.com>
2016-09-21 17:11:35 +0100

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -2,6 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(req,res) {
   res.writeHead(200);
-  res.end('Hello World from OSE!');
+  res.end('Hello World from OSE part2!');
 });
 server.listen(8080);
